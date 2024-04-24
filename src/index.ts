f30c51df14bb392a6bea3aeb935fbcd84f4ff3fd
Darpan Vats <dva@protechnology.de>
2024-04-24 12:31:47 +0200

    - cleaning code

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,22 @@
-import mongoose, { Mongoose } from 'mongoose';
+import { ApolloServer } from "@apollo/server";
+import mongoose from "mongoose";
+import { resolvers } from "./graphql/resolvers";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./graphql/typeDefs";
 
-import { ApolloServer } from '@apollo/server';
-import { resolvers } from './graphql/resolvers';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './graphql/typeDefs';
+const MONGODB =
+  "mongodb+srv://mdarpanvats:bN85qw1JUB3W0zuT@recipecluster.fma79cd.mongodb.net/?retryWrites=true&w=majority&appName=RecipeCluster";
 
-const MONGODB = "mongodb+srv://mdarpanvats:bN85qw1JUB3W0zuT@recipecluster.fma79cd.mongodb.net/?retryWrites=true&w=majority&appName=RecipeCluster";
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-
-  // Resolvers define how to fetch the types defined in your schema.
-
-  // The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
-  typeDefs,  
+  typeDefs,
   resolvers,
-  });
-  
-  // Passing an ApolloServer instance to the `startStandaloneServer` function:
-  
-  // console.log(`🚀  Server ready at: ${url}`);
+});
 
-  mongoose.connect( MONGODB)
-.then(() => {
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+
+mongoose.connect(MONGODB).then(() => {
   console.log("connection to MongoDB successful");
-  return startStandaloneServer( server, {
-    listen: { port: 4000}
-  }).then((res) => 
-  console.log(`Listening to ${res.url}`))
-})
+  return startStandaloneServer(server, {
+    listen: { port: 4000 },
+  }).then((res) => console.log(`Listening to ${res.url}`));
+});
